e2be32425d73f156367bf783cf65505e67887120
slipperyduckza <slipperyduck@grrr.co.za>
2025-10-29 12:29:35 +0200

    Update Windows executable metadata

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winresource::WindowsResource::new();
-        res.set("FileDescription", "LibreHardware Prototype");
-        res.set("ProductName", "LibreHardware Prototype");
-        res.set("CompanyName", "Your Company");
+        res.set("FileDescription", "CuteMonitor");
+        res.set("ProductName", "CuteMonitor");
+        res.set("CompanyName", "SlipperyduckZA");
         res.set_version_info(winresource::VersionInfo::FILEVERSION, 0x0000000000010000);
         res.set_version_info(winresource::VersionInfo::PRODUCTVERSION, 0x0000000000010000);
         res.set_icon("cutemonitor.ico");
